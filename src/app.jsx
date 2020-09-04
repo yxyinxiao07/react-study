d602c1486d6b41ac0fe2ffa07c799acb86c1f8e3
yxyinxiao07 <1874574983@qq.com>
2020-09-04 17:30:01 +0800

    todomvc

--- src/app.jsx
+++ src/app.jsx
@@ -1,13 +1,11 @@
 import React from "react";
-import Nav from "./nav";
+import Todomvc from "./todomvc";
 
 class App extends React.Component {
   render() {
-    var msg = ['视频','音乐','英语','美术']
     return  (
       <div>
-        <h1>123</h1>
-        <Nav navlist={msg}/>
+        <Todomvc/>
       </div>
     )
   }
